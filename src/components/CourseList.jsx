0f937c7f4fc9b561ac14e2aa1dff4ca3e93f56b6
jamie7533 <lee.jamie.july@gmail.com>
2022-09-30 11:48:34 -0500

    term filter

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
+const TermSelector = ({ selection, setSelection }) => (
+  <div className="btn-group">
+    {
+      Object.values(terms)
+        .map(value => <TermButton key={value} term={value} selection={selection} setSelection={setSelection} />)
+    }
+  </div>
+);
+const TermButton = ({ term, selection, setSelection }) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
+      onChange={() => setSelection(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+      {term}
+    </label>
+  </>
+);
+
 const Course = ({ course }) => (
   <div className="card m-1 p-2">
     <div className="card-body">
@@ -11,10 +32,15 @@ const Course = ({ course }) => (
 );
 
 const CourseList = ({ courses }) => {
+  const [selection, setSelection] = useState(() => Object.values(terms)[0])
+  const termCourses = Object.values(courses).filter(course => selection === course.term);
   return (
-    <div className="course-list">
-      {Object.entries(courses).map(([id, course]) => <Course key={id} course={course} />)}
-    </div>
+    <>
+      <TermSelector selection={selection} setSelection={setSelection} />
+      <div className="course-list">
+        {Object.entries(termCourses).map(([id, course]) => <Course key={id} course={course} />)}
+      </div>
+    </>
   );
 };
 
