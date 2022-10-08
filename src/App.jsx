cb85cf174eb59ea60c3284db9863c062ee010016
jamie7533 <lee.jamie.july@gmail.com>
2022-10-08 14:12:46 -0500

    catch time conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Banner from "./components/Banner.jsx";
 import CourseList from "./components/CourseList.jsx";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch.js';
+import { addScheduleTimes } from './utilities/time';
 
 const Main = () => {
   const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+  let schedule = [];
 
   if (error) return <h1>Error loading course data: {`${error}`}</h1>;
   if (isLoading) return <h1>Loading course data...</h1>;
   if (!data) return <h1>No course data found</h1>;
-
+  schedule = addScheduleTimes(data);
   return (
     <>
-    <Banner title={data.title}/>
-    <CourseList courses={data.courses}/>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
     </>
   );
 }
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import Modal from './Modal';
+import Modal from "./Modal";
 import Cart from "./Cart";
+import Course from "./Course";
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
+
 const TermSelector = ({ selection, setSelection }) => (
   <div className="btn-group">
     {
@@ -11,6 +13,7 @@ const TermSelector = ({ selection, setSelection }) => (
     }
   </div>
 );
+
 const TermButton = ({ term, selection, setSelection }) => (
   <>
     <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
@@ -24,25 +27,6 @@ const TermButton = ({ term, selection, setSelection }) => (
 const ScheduleButton = ({ openModal }) => (
   <button className="ms-auto btn btn-dark" onClick={openModal}>Course Plan</button>
 );
-
-const Course = ({ course, selected, toggleSelected }) => {
-  const isSelected = selected.includes(course);
-  const style = {
-    backgroundColor: isSelected ? 'lemonchiffon' : 'white',
-    color: isSelected ? 'green' : 'black'
-  };
-  return (
-    <div className="card m-1 p-2" onClick={() => { toggleSelected(course) }} style={style}>
-      <div className="card-body">
-        <h5 className="card-title">{course.term} CS {course.number}</h5>
-        <div className="card-text">{course.title}</div>
-      </div>
-      <div className="card-footer">
-        {course.meets}
-      </div>
-    </div>
-  );
-};
 
 const CourseList = ({ courses }) => {
   const [selection, setSelection] = useState(() => Object.values(terms)[0])
